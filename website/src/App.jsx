aeeb8e25e5b847df0d6897e2a02afa7b37ace354
khelif96 <khelif96@gmail.com>
2018-10-13 18:40:00 -0400

    Revereted to a PureComponent from a purefunction

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
+/* eslint-disable react/prefer-stateless-function */
+import React, { PureComponent } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Home } from './views';
 import './App.css';
 
-const App = () => (
-  <Router>
-    <div style={{ width: '100%', height: '100%' }}>
-      <Route exact path="/" component={Home} />
-      {/* Example for route
-        <Route path="/...." component={ComponentName} />
-      */}
-    </div>
-  </Router>
-);
+class App extends PureComponent {
+  render() {
+    return (
+      <Router>
+        <div style={{ width: '100%', height: '100%' }}>
+          <Route exact path="/" component={Home} />
+          {/* Example for route
+            <Route path="/...." component={ComponentName} />
+          */}
+        </div>
+      </Router>
+    );
+  }
+}
+
 export default App;
