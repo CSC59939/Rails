a9dfec516f505d62ea9c13d01ea14e0b96f8b5fe
khelif96 <khelif96@gmail.com>
2018-10-19 00:50:57 -0400

    Dont stylelint jsx

--- website/.eslintrc.js
+++ website/.eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     "extends": "airbnb",
     "parser": "babel-eslint",
     "rules": {
-      "react/prefer-stateless-function": [2, { "ignorePureComponents": true }]
+      "react/prefer-stateless-function": [2, { "ignorePureComponents": true }],
+      "no-unused-vars": 2
     }
 };
