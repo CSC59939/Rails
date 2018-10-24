d5dd63f296f287b656ac3ed5a7f3531c82084b63
Tobias He <xtobiashe@gmail.com>
2018-10-24 00:37:14 -0400

    modified styling, added cards

--- website/src/components/Notification/Notification.jsx
+++ website/src/components/Notification/Notification.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Drawer, Button } from 'antd';
+import { Drawer, Button, Card } from 'antd';
 
 class Notification extends React.Component {
   state = { visible: false };
@@ -17,22 +17,34 @@ class Notification extends React.Component {
   };
 
   render() {
+    const notificationArray = [
+      {
+        title: 'Notification 1',
+        message: 'Message 1',
+      },
+      {
+        title: 'Notification 2',
+        message: 'Message 2',
+      },
+    ];
     return (
       <div>
         <Button type="primary" onClick={this.showDrawer}>
           Open
         </Button>
         <Drawer
-          title="Basic Drawer"
-          width="1000"
+          title="Notifications"
           placement="right"
+          width="500"
           closable={false}
           onClose={this.onClose}
           visible={this.state.visible}
         >
-          <p>Some contents...</p>
-          <p>Some contents...</p>
-          <p>Some contents...</p>
+          {notificationArray.map(data => (
+            <Card title={data.title} style={{ marginBottom: 25, borderRadius: 30, width: 350 }}>
+              <p>{data.message}</p>
+            </Card>
+          ))}
         </Drawer>
       </div>
     );
