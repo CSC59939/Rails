7b7df2ff55a15355e1bc26c96ee41c3381350261
whuang001 <vincentwongad@gmail.com>
2018-11-19 23:44:09 -0500

    add forgot password API and VIEW

--- website/src/views/Signin/Signin.jsx
+++ website/src/views/Signin/Signin.jsx
@@ -1,5 +1,5 @@
 import {
-  Button, Card, Icon, Input, message,
+  Button, Card, Icon, Input, message, Modal,
 } from 'antd';
 import React, { PureComponent } from 'react';
 import firebase from 'firebase/app';
@@ -17,8 +17,13 @@ export default class Signin extends PureComponent {
       email: '',
       password: '',
       loading: false,
+      /* Set pop up window to be unvisible*/
+      visible: false,
     };
     this.signin = this.signin.bind(this);
+    this.forgotPassword = this.forgotPassword.bind(this);
+    this.handleOk = this.handleOk.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
   }
 
   signin() {
@@ -43,8 +48,39 @@ export default class Signin extends PureComponent {
       });
   }
 
+/* Pop out a forgot password window */
+  forgotPassword = () => {
+    this.setState({
+      visible: true,
+    });
+  }
+
+/* Handle cancel button in the forgot password window */
+  handleCancel = () => {
+    this.setState({
+      visible: false,
+    });
+  }
+
+/* Handle ok button in the forgot password window, send user a password reset email using firebase function */
+  handleOk() {
+    const { email } =  this.state;
+    if (email === '') {
+      message.error('Please input your email address.');
+      return;
+    }
+    firebase.auth().sendPasswordResetEmail(email).then(function() {
+      message.success('Password reset email has been sent.')
+    }).catch(function(error) {
+      message.error(err.message);
+    });
+    this.setState({
+      visible: false,
+    });
+}
+ 
   render() {
-    const { email, password, loading } = this.state;
+    const { email, password, loading, visible } = this.state;
     const { history } = this.props;
     const user = firebase.auth().currentUser;
     if (user) {
@@ -85,6 +121,28 @@ export default class Signin extends PureComponent {
             <Button size="large" block style={{ marginTop: 10 }} href="/signup">
               <p style={{ margin: 0, display: 'inline' }}>Don&#39;t have an account? Sign Up.</p>
             </Button>
+            <Button size="large" block style={{ marginTop: 10 }} onClick={this.forgotPassword}>
+              <p style={{ margin: 0, display: 'inline' }}>Forgot Password?</p>
+            </Button>
+            <Modal
+              title="Forgot password"
+              className="forgot-password-modal"
+              visible={visible}
+              onOk={this.handleOk}
+              onCancel={this.handleCancel}
+            >
+              <p className="forgot-password-label">Please enter your E-mail address, we will send you a password reset email:</p>
+              <Input
+                required
+                size="large"
+                placeholder="E-Mail"
+                prefix={<Icon type="mail" />}
+                style={{ marginTop: 10 }}
+                type="email"
+                value={email}
+                onChange={(e) => { this.setState({ email: e.target.value }); }}
+              />
+            </Modal>
           </div>
         </Card>
       </div>
