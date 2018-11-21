2c158af9530882c3db4fe5aebbdb53cc9695ab0c
LiuYuHao <liuyuhao717@gmail.com>
2018-11-21 01:42:40 -0500

    rebase from the master, and remodified all the snapshot cases, using the mock firebase and fix stylelint of each cases

---
+++ website/src/views/Signup/Signuptest.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {
+  configure,
+} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import firebase from 'firebase';
+import { Signup } from '..';
+
+configure({ adapter: new Adapter() });
+
+
+describe('Signup', () => {
+  jest.spyOn(firebase, 'auth')
+    .mockImplementation(() => ({
+      currentUser: {
+        displayName: 'testDisplayName',
+        email: 'test@test.com',
+        emailVerified: true,
+        getIdToken: () => new Promise(((resolve) => {
+          setTimeout(() => {
+            resolve(1234);
+          }, 0);
+        })),
+      },
+    }));
+  it('Render Signup without any eror', () => { // eslint-disable-line no-undef
+    expect(renderer.create(<Signup />).toJSON()).toMatchSnapshot();
+  });
+});
