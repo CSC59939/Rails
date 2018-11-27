bf986e6fb1dada04676ca7736718cda4ff5d37e2
LiuYuHao <liuyuhao717@gmail.com>
2018-11-26 20:55:11 -0500

    small change based on the comment from my teammate, remove useless comments and change layout

--- website/src/components/MaterialIcon/MaterialIcon.test.jsx
+++ website/src/components/MaterialIcon/MaterialIcon.test.jsx
@@ -18,9 +18,11 @@ describe('MaterialIcon', () => {
   it('Expect MaterialIcon to return an icon with a settings cog with a custom class', () => {
     expect(renderer.create(<MaterialIcon className="customClassnpm" type="settings" />).toJSON()).toMatchSnapshot();
   });
+
   it('should contains a i tag with corresponding classname and type with text type argument(settings)', () => {
     expect(shallow(<MaterialIcon className="customClassnpm" type="settings" />).contains(<i style={null} className="customClassnpm material-icons">settings</i>)).toBe(true);
   });
+
   it('should have a class name called customClassnpm(from argument)', () => {
     expect(shallow(<MaterialIcon className="customClassnpm" type="settings" />).exists('.customClassnpm')).toBe(true);
   });
