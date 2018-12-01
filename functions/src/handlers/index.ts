adf73048a3a77ef098b58c3c596b706f125bffed
Tobias He <xtobiashe@gmail.com>
2018-12-01 17:32:07 -0500

    Created two new APIs
    Added two new APIs
    1. EditClassHandler
    2. GetClassDetailHandler

--- functions/src/handlers/index.ts
+++ functions/src/handlers/index.ts
@@ -1,23 +1,27 @@
-import { approveclass } from './ApproveClassHandler';
-import { createclass } from './CreateClassHandler';
-import { getclasses } from './GetClassesHandler';
-import { getprofile } from './GetProfileHandler';
-import { joinclass } from './JoinClassHandler';
-import { requestclass } from './RequestClassHandler';
-import { signup } from './SignupHandler';
-import { createevent } from './CreateEventHandler';
-import { editevent } from './EditEventHandler';
-import { geteventdetails } from './GetEventDetailHandler';
+import { approveclass } from "./ApproveClassHandler";
+import { createclass } from "./CreateClassHandler";
+import { getclasses } from "./GetClassesHandler";
+import { getprofile } from "./GetProfileHandler";
+import { joinclass } from "./JoinClassHandler";
+import { requestclass } from "./RequestClassHandler";
+import { signup } from "./SignupHandler";
+import { createevent } from "./CreateEventHandler";
+import { editevent } from "./EditEventHandler";
+import { getclassdetails } from "./GetClassDetailHandler";
+import { geteventdetails } from "./GetEventDetailHandler";
+import { editclass } from "./EditClassHandler";
 
 export const handlers = {
-    approveclass,
-    createclass,
-    getclasses,
-    getprofile,
-    joinclass,
-    requestclass,
-    signup,
-    createevent,
-    editevent,
-    geteventdetails,
+  approveclass,
+  createclass,
+  getclasses,
+  getprofile,
+  joinclass,
+  requestclass,
+  signup,
+  createevent,
+  editevent,
+  getclassdetails,
+  geteventdetails,
+  editclass
 };
