efd65f183bc7b61b9a43fbcde499895a774a36ff
LiuYuHao <liuyuhao717@gmail.com>
2018-12-01 15:48:09 -0500

    change the name to better understanding

--- website/src/components/IconButton/IconButton.test.jsx
+++ website/src/components/IconButton/IconButton.test.jsx
@@ -21,38 +21,38 @@ describe('IconButton', () => {
     expect(tree).toMatchSnapshot();
   });
   it('should have a type called settings', () => {
-    const sampleFunctionfn = jest.fn(sampleFunction);
-    const wrapper = shallow(<IconButton type="settings" onClick={sampleFunctionfn} />);
+    const sampleFunctionMock = jest.fn(sampleFunction);
+    const wrapper = shallow(<IconButton type="settings" onClick={sampleFunctionMock} />);
     expect(wrapper.find({ type: 'settings' }).length).toBe(1);
     wrapper.find('button').simulate('click');
-    expect(sampleFunctionfn.mock.calls.length).toBe(1);
+    expect(sampleFunctionMock.mock.calls.length).toBe(1);
   });
   it('should have a type called add_box', () => {
-    const sampleFunctionfn = jest.fn(sampleFunction);
-    const wrapper = shallow(<IconButton type="add_box" onClick={sampleFunctionfn} />);
+    const sampleFunctionMock = jest.fn(sampleFunction);
+    const wrapper = shallow(<IconButton type="add_box" onClick={sampleFunctionMock} />);
     expect(wrapper.find({ type: 'add_box' }).length).toBe(1);
     wrapper.find('button').simulate('click');
-    expect(sampleFunctionfn.mock.calls.length).toBe(1);
+    expect(sampleFunctionMock.mock.calls.length).toBe(1);
   });
   it('should have a type called add_alert', () => {
-    const sampleFunctionfn = jest.fn(sampleFunction);
-    const wrapper = shallow(<IconButton type="add_alert" onClick={sampleFunctionfn} />);
+    const sampleFunctionMock = jest.fn(sampleFunction);
+    const wrapper = shallow(<IconButton type="add_alert" onClick={sampleFunctionMock} />);
     expect(wrapper.find({ type: 'add_alert' }).length).toBe(1);
     wrapper.find('button').simulate('click');
-    expect(sampleFunctionfn.mock.calls.length).toBe(1);
+    expect(sampleFunctionMock.mock.calls.length).toBe(1);
   });
   it('should have a type called exit_to_app', () => {
-    const sampleFunctionfn = jest.fn(sampleFunction);
-    const wrapper = shallow(<IconButton type="exit_to_app" onClick={sampleFunctionfn} />);
+    const sampleFunctionMock = jest.fn(sampleFunction);
+    const wrapper = shallow(<IconButton type="exit_to_app" onClick={sampleFunctionMock} />);
     expect(wrapper.find({ type: 'exit_to_app' }).length).toBe(1);
     wrapper.find('button').simulate('click');
-    expect(sampleFunctionfn.mock.calls.length).toBe(1);
+    expect(sampleFunctionMock.mock.calls.length).toBe(1);
   });
   it('should have a type called notifications_active', () => {
-    const sampleFunctionfn = jest.fn(sampleFunction);
-    const wrapper = shallow(<IconButton type="notifications_active" onClick={sampleFunctionfn} />);
+    const sampleFunctionMock = jest.fn(sampleFunction);
+    const wrapper = shallow(<IconButton type="notifications_active" onClick={sampleFunctionMock} />);
     expect(wrapper.find({ type: 'notifications_active' }).length).toBe(1);
     wrapper.find('button').simulate('click');
-    expect(sampleFunctionfn.mock.calls.length).toBe(1);
+    expect(sampleFunctionMock.mock.calls.length).toBe(1);
   });
 });
